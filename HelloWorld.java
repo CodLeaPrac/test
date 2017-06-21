5559e8db35eeb0841a8a92b2822b3b44caf929f5
CodLeaPrac <yangtianchangtan@126.com>
2017-06-21 15:18:21 +0800

    this is java program

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+*
+* use java language pring "Hellow world" to console
+*
+/
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
+}
